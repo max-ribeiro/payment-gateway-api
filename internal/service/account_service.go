52fc17277a41c2d0bbfc083081a071875dd97dee
max-ribeiro <max_ribeiro@live.com>
2025-04-08 17:47:56 -0300

    feat: crete the service for account

--- internal/service/account_service.go
+++ internal/service/account_service.go
@@ -1,7 +1,69 @@
 package service
 
-import "github.com/devfullcycle/imersao22/go-gateway/internal/domain"
+import (
+	"github.com/devfullcycle/imersao22/go-gateway/internal/domain"
+	"github.com/devfullcycle/imersao22/go-gateway/internal/dto"
+)
 
 type AccountService struct {
 	repository domain.AccountRepository
+}
+
+func NewAccountService(repository domain.AccountRepository) *AccountService {
+	return &AccountService{repository: repository}
+}
+
+func (service *AccountService) CreateAccount(input dto.CreateAccountInput) (*dto.AccountOutput, error) {
+	account := dto.ToAccount(input)
+	existingAccount, err := service.repository.FindByAPIKey(account.APIKey)
+	if err != nil && err != domain.ErrAccountNotFound {
+		return nil, err
+	}
+	if existingAccount != nil {
+		return nil, domain.ErrDuplicatedAPIKey
+	}
+	err = service.repository.Save(account)
+
+	if err != nil {
+		return nil, err
+	}
+
+	output := dto.FromAccount(account)
+	return &output, nil
+}
+
+func (service *AccountService) UpdateBalance(apiKey string, amount float64) (*dto.AccountOutput, error) {
+	account, err := service.repository.FindByAPIKey(apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	account.AddBalance(amount)
+	err = service.repository.UpdateBalance(account)
+	if err != nil {
+		return nil, err
+	}
+
+	output := dto.FromAccount(account)
+	return &output, nil
+}
+
+func (service *AccountService) FindByAPIKey(apiKey string) (*dto.AccountOutput, error) {
+	account, err := service.repository.FindByAPIKey(apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	output := dto.FromAccount(account)
+	return &output, nil
+}
+
+func (service *AccountService) FindByID(id string) (*dto.AccountOutput, error) {
+	account, err := service.repository.FindByID(id)
+	if err != nil {
+		return nil, err
+	}
+
+	output := dto.FromAccount(account)
+	return &output, nil
 }
